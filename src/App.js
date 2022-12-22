0a26ed3c5a01d31128d66d2a592f825d17658890
Serge Seva <sergeseva@catalyst.net.nz>
2022-12-23 08:28:03 +1300

    Add initial files structure.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="content">
+      <header className="header__primary">
+        <h1 className="title__primary">Medialess template</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;

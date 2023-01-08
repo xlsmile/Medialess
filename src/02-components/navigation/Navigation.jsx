8d3b809c9ca6de24a4ed5944bccd0daff3c1dae8
Serge <protomix@gmail.com>
2023-01-08 14:51:23 +1300

    Build main nav start.

--- src/02-components/navigation/Navigation.jsx
+++ src/02-components/navigation/Navigation.jsx
@@ -1,5 +1,50 @@
 const Navigation = () => {
-  return <nav className="navigation__main">Navigation</nav>;
+  return (
+    <nav className="navigation">
+      <div className="navigation__group">
+        <ul className="list">
+          <li className="list__item">
+            <a className="list__link" href="/">
+              AIW
+            </a>
+          </li>
+          <li className="list__item">
+            <a className="list__link" href="/">
+              Home
+            </a>
+          </li>
+          <li className="list__item">
+            <a className="list__link" href="/">
+              What is AIW
+            </a>
+          </li>
+          <li className="list__item">
+            <a className="list__link" href="/">
+              Open AI
+            </a>
+          </li>
+          <li className="list__item">
+            <a className="list__link" href="/">
+              Case studies
+            </a>
+          </li>
+          <li className="list__item">
+            <a className="list__link" href="/">
+              Library
+            </a>
+          </li>
+        </ul>
+        <ul className="list">
+          <li className="list__item">
+            <button>Sign in</button>
+          </li>
+          <li className="list__item">
+            <button>Sign up</button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navigation;

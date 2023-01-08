34c957caee9bd4f443967fc7e092fd715af90cc3
Serge <protomix@gmail.com>
2023-01-08 15:39:19 +1300

    Update Partnership block.

--- src/02-components/header/Header.jsx
+++ src/02-components/header/Header.jsx
@@ -3,7 +3,6 @@ const Header = () => {
     <header className="header">
       <h1>Let’s Build Something amazing with AIO</h1>
       <p>
-        {' '}
         Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way
         everything joy alteration boisterous the attachment. Party we years to order allow asked of.
       </p>

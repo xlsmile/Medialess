e23fbc9123a1dc49f6b3a0b6f7316b0fc806c473
Serge Seva <sergeseva@catalyst.net.nz>
2023-01-10 12:52:45 +1300

    Update main header.

--- src/02-components/header/Header.jsx
+++ src/02-components/header/Header.jsx
@@ -1,21 +1,28 @@
+import people from '../../assets/people.png';
+import head from '../../assets/head.png';
+
 const Header = () => {
   return (
     <header className="header">
-      <h1>Let’s Build Something amazing with AIO AI Open</h1>
-      <p>
-        Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way
-        everything joy alteration boisterous the attachment. Party we years to order allow asked of.
-      </p>
-      <form>
-        <label htmlFor="email">Your Email Address</label>
-        <input id="email" type="email" name="email" placeholder="Your Email Address" />
-        <button type="submit">Get started</button>
-      </form>
-      <div>
-        <img src="/" alt="users avatars" />
-        <p>1,600 people requested access a visit in last 24 hours</p>
+      <div className="header__blurb">
+        <h1>Let's Build Something amazing with AIO AI Open</h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence
+          way everything joy alteration boisterous the attachment. Party we years to order allow asked of.
+        </p>
+        <form>
+          <label htmlFor="email">Your Email Address</label>
+          <input id="email" type="email" name="email" placeholder="Your Email Address" />
+          <button type="submit">Get started</button>
+        </form>
+        <div>
+          <img src={people} alt="users avatars" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
       </div>
-      <img src="/" alt="ai head illustration" />
+      <div className="header__illustration">
+        <img className="header__image" src={head} alt="ai head illustration" />
+      </div>
     </header>
   );
 };

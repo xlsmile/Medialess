c5fe277fc13d7625f20096dc7a4e4d46d2cfa475
Serge Seva <sergeseva@catalyst.net.nz>
2023-01-10 14:26:48 +1300

    Tweak utilities scss.

--- src/02-components/header/Header.jsx
+++ src/02-components/header/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
           <button type="submit">Get started</button>
         </form>
         <div>
-          <img className="u-margin-bottom--sm" src={people} alt="users avatars" />
+          <img className="margin-bottom--sm" src={people} alt="users avatars" />
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
       </div>

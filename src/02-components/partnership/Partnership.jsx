c897102d397657a242f0dc88d8f9dbec8454ec88
Serge Seva <sergeseva@catalyst.net.nz>
2023-01-11 09:53:52 +1300

    Add Partnership Component.

--- src/02-components/partnership/Partnership.jsx
+++ src/02-components/partnership/Partnership.jsx
@@ -1,7 +1,13 @@
+import { google, slack, atlassian, dropbox, shopify } from './partners';
+
 const Partnership = () => {
   return (
     <div className="partnership">
-      <p>Partnership</p>
+      <img src={google} alt={`${google} logo`} />
+      <img src={slack} alt={`${slack} logo`} />
+      <img src={atlassian} alt={`${atlassian} logo`} />
+      <img src={dropbox} alt={`${dropbox} logo`} />
+      <img src={shopify} alt={`${shopify} logo`} />
     </div>
   );
 };

009572e1718337f72851b845632ec106d44c26d8
Serge Seva <sergeseva@catalyst.net.nz>
2023-01-11 13:29:55 +1300

    Update Info Component UI.

--- src/02-components/info/Info.jsx
+++ src/02-components/info/Info.jsx
@@ -7,24 +7,26 @@ const Info = () => {
         deadlights crow's nest snow squiffy furl booty skysail. Ho carouser reef Privateer parley league gaff gunwalls
         cutlass hail-shot.
       </p>
-      <h3>The possibilities are beyond your imagination</h3>
-      <a href="/">Explore The Library</a>
-      <div>
-        <h4 className="info__feature-title margin-bottom--xs">Chatbots</h4>
+      <h3 className="info__feature-title--sub">The possibilities are beyond your imagination</h3>
+      <a className="info__feature-link" href="/">
+        Explore The Library
+      </a>
+      <div className="info__card">
+        <h4 className="info__card-title">Chatbots</h4>
         <p>
           Run a rig boatswain long boat code of conduct grog gaff ballast topmast chase Blimey. Tender overhaul mutiny
           fluke scourge of the seven seas driver killick no prey, no pay stern gun.
         </p>
       </div>
-      <div>
-        <h4 className="info__feature-title margin-bottom--xs">Knowledgebase</h4>
+      <div className="info__card">
+        <h4 className="info__card-title">Knowledgebase</h4>
         <p>
           Handsomely spyglass gabion draught Jack Ketch cog fire in the hole shrouds to go on account loaded to the
           gunwalls. Spike heave to topsail hogshead Yellow Jack plunder sloop grapple spanker fathom.
         </p>
       </div>
-      <div>
-        <h4 className="info__feature-title margin-bottom--xs">Education</h4>
+      <div className="info__card">
+        <h4 className="info__card-title">Education</h4>
         <p>
           Dead men tell no tales nipper yard furl knave clap of thunder carouser Blimey crimp hornswaggle. Transom hulk
           ye brig boatswain Brethren of the Coast scallywag killick rigging maroon.

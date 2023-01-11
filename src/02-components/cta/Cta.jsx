8c085726d851814296d087055980fa70a564d3e1
Serge Seva <sergeseva@catalyst.net.nz>
2023-01-11 14:55:55 +1300

    Style Cta Component.

--- src/02-components/cta/Cta.jsx
+++ src/02-components/cta/Cta.jsx
@@ -1,7 +1,13 @@
+import Button from '../button/Button';
+
 const Cta = () => {
   return (
     <div className="cta">
-      <p>Cta</p>
+      <span className="dvd">
+        <h5 className="cta__title">Request Early Access to Get Started</h5>
+        <p className="cta__text">Register today & start exploring the endless possiblities.</p>
+      </span>
+      <Button className="button button__rounded--cta">Get started</Button>
     </div>
   );
 };

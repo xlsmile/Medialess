081e0f12b1af64b68b756a655b100611168b5bae
Serge Seva <sergeseva@catalyst.net.nz>
2023-01-12 07:00:05 +1300

    Update Partneship Component items.

--- src/02-components/partnership/Partnership.jsx
+++ src/02-components/partnership/Partnership.jsx
@@ -2,13 +2,35 @@ import { google, slack, atlassian, dropbox, shopify } from './partners';
 
 const Partnership = () => {
   return (
-    <div className="partnership">
-      <img src={google} alt={`${google} logo`} />
-      <img src={slack} alt={`${slack} logo`} />
-      <img src={atlassian} alt={`${atlassian} logo`} />
-      <img src={dropbox} alt={`${dropbox} logo`} />
-      <img src={shopify} alt={`${shopify} logo`} />
-    </div>
+    <nav className="partnership">
+      <ul className="partnership__list">
+        <li className="partnership__list-item">
+          <a className="partnership__list-link" href="/">
+            <img className="partnership__list-image" src={google} alt={`${google} logo`} />
+          </a>
+        </li>
+        <li className="partnership__list-item">
+          <a className="partnership__list-link" href="/">
+            <img className="partnership__list-image" src={slack} alt={`${slack} logo`} />
+          </a>
+        </li>
+        <li className="partnership__list-item">
+          <a className="partnership__list-link" href="/">
+            <img className="partnership__list-image" src={atlassian} alt={`${atlassian} logo`} />
+          </a>
+        </li>
+        <li className="partnership__list-item">
+          <a className="partnership__list-link" href="/">
+            <img className="partnership__list-image" src={dropbox} alt={`${dropbox} logo`} />
+          </a>
+        </li>
+        <li className="partnership__list-item">
+          <a className="partnership__list-link" href="/">
+            <img className="partnership__list-image" src={shopify} alt={`${shopify} logo`} />
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 };
 

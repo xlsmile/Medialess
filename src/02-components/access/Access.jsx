67ecfb0ed22094e3ceff3c780cdb8b2f1e788bf3
Serge Seva <sergeseva@catalyst.net.nz>
2023-01-16 12:41:17 +1300

    Build Blog UI.

--- src/02-components/access/Access.jsx
+++ src/02-components/access/Access.jsx
@@ -1,8 +1,8 @@
 const Access = () => {
   return (
     <section className="access" id="access">
-      <h2>Request Early Access to Get Started</h2>
-      <h3>The possibilities are beyond your imagination</h3>
+      <h2 className="access__title">Request Early Access to Get Started</h2>
+      <h3 className="access__titlesub">The possibilities are beyond your imagination</h3>
       <p>
         Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way
         everything joy alteration boisterous the attachment. Party we years to order allow asked of.
--- src/02-components/blog/Blog.jsx
+++ src/02-components/blog/Blog.jsx
@@ -1,21 +1,72 @@
+import { blog01, blog02, blog03, blog04, blog05 } from './images';
+
 const Blog = () => {
+  const timestamp = new Date().toDateString();
+
   return (
     <section className="blog" id="blog">
-      <h2>A lot is happening, We are blogging about it.</h2>
-      <article>
-        <p>Article 01</p>
+      <h2 className="blog__title">A lot is happening, We are blogging about it.</h2>
+      <article className="blog-post blog-post--featured">
+        <img className="blog-post__image" src={blog01} alt={`${blog01} logo`} />
+        <div className="blog-post__content">
+          <span>
+            <span className="blog-post__date">{timestamp}</span>
+            <h3 className="blog-post__title">
+              AIO is the future. Let us exlore how it is. AIO Round spanker hempen halter.
+            </h3>
+          </span>
+          <a className="blog-post__link" href="/">
+            Read full article
+          </a>
+        </div>
       </article>
-      <article>
-        <p>Article 02</p>
+      <article className="blog-post">
+        <img className="blog-post__image" src={blog02} alt={`${blog02} logo`} />
+        <div className="blog-post__content">
+          <span>
+            <span className="blog-post__date">{timestamp}</span>
+            <h3 className="blog-post__title">Pinnace AIO mizzenmast quarter nest.</h3>
+          </span>
+          <a className="blog-post__link" href="/">
+            Read full article
+          </a>
+        </div>
       </article>
-      <article>
-        <p>Article 03</p>
+      <article className="blog-post">
+        <img className="blog-post__image" src={blog03} alt={`${blog03} logo`} />
+        <div className="blog-post__content">
+          <span>
+            <span className="blog-post__date">{timestamp}</span>
+            <h3 className="blog-post__title">Shiver me timbers to go on account lookout wherry.</h3>
+          </span>
+          <a className="blog-post__link" href="/">
+            Read full article
+          </a>
+        </div>
       </article>
-      <article>
-        <p>Article 04</p>
+      <article className="blog-post">
+        <img className="blog-post__image" src={blog04} alt={`${blog04} logo`} />
+        <div className="blog-post__content">
+          <span>
+            <span className="blog-post__date">{timestamp}</span>
+            <h3 className="blog-post__title">Case shot Shiver me timbers gangplank.</h3>
+          </span>
+          <a className="blog-post__link" href="/">
+            Read full article
+          </a>
+        </div>
       </article>
-      <article>
-        <p>Article 05</p>
+      <article className="blog-post">
+        <img className="blog-post__image" src={blog05} alt={`${blog05} logo`} />
+        <div className="blog-post__content">
+          <span>
+            <span className="blog-post__date">{timestamp}</span>
+            <h3 className="blog-post__title">Rigging hempen captain killsick.</h3>
+          </span>
+          <a className="blog-post__link" href="/">
+            Read full article
+          </a>
+        </div>
       </article>
     </section>
   );

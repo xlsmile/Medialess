5bd9857ef792f33ce9a1942f8b25b96ff1eadec6
Serge Seva <sergeseva@catalyst.net.nz>
2023-01-17 12:18:46 +1300

    Create markup for Footer component.

--- src/02-components/footer/Footer.jsx
+++ src/02-components/footer/Footer.jsx
@@ -4,7 +4,52 @@ const Footer = () => {
   return (
     <footer className="footer__main">
       <h2>Do you want to step in to the future before others</h2>
-      <Button className="button button__rounded">Request Early Access</Button>
+      <Button className="button button__rounded--hollow">Request Early Access</Button>
+      <div>
+        <h4>AIO</h4>
+        <p>Bla-bla-bla-bla-bla</p>
+      </div>
+      <div>
+        <h5>Links</h5>
+        <ul>
+          <li>
+            <a href="/">Overons</a>
+          </li>
+          <li>
+            <a href="/">Social meida</a>
+          </li>
+          <li>
+            <a href="/">Counters</a>
+          </li>
+          <li>
+            <a href="/">Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h5>Company</h5>
+        <ul>
+          <li>
+            <a href="/">Terms & conditions</a>
+          </li>
+          <li>
+            <a href="/">Privacy policy</a>
+          </li>
+          <li>
+            <a href="/">Whatever</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h5>Get in touch</h5>
+        <ul>
+          <li>Lorem ipsum dolor sit.</li>
+          <li>085-20923-87</li>
+          <li>
+            <a href="mailto:info@info.com">info@info.com</a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 };

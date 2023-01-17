14da998b6c23a0353be2edb053a13428f6f09bc3
Serge Seva <sergeseva@catalyst.net.nz>
2023-01-17 14:35:09 +1300

    Update Footer UI.

--- src/02-components/footer/Footer.jsx
+++ src/02-components/footer/Footer.jsx
@@ -3,57 +3,70 @@ import Button from '../button/Button';
 const Footer = () => {
   return (
     <footer className="footer" id="footer">
-      <div className="footer__top">
-        <h2>Do you want to step in to the future before others</h2>
-        <Button className="button button__rounded--hollow">Request Early Access</Button>
+      <h2>Do you want to step in to the future before others</h2>
+      <Button className="button button__rounded--hollow">Request Early Access</Button>
+
+      <div className="footer__navigation-intro">
+        <h4>AIO</h4>
+        <p>Bla-bla-bla-bla-bla</p>
       </div>
-      <nav className="footer__navigation">
-        <div className="footer__navigation-intro">
-          <h4>AIO</h4>
-          <p>Bla-bla-bla-bla-bla</p>
-        </div>
-        <div>
-          <h5>Links</h5>
-          <ul>
-            <li>
-              <a href="/">Overons</a>
-            </li>
-            <li>
-              <a href="/">Social meida</a>
-            </li>
-            <li>
-              <a href="/">Counters</a>
-            </li>
-            <li>
-              <a href="/">Contact</a>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <h5>Company</h5>
-          <ul>
-            <li>
-              <a href="/">Terms & conditions</a>
-            </li>
-            <li>
-              <a href="/">Privacy policy</a>
-            </li>
-            <li>
-              <a href="/">Whatever</a>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <h5>Get in touch</h5>
-          <ul>
-            <li>Lorem ipsum dolor sit.</li>
-            <li>085-20923-87</li>
-            <li>
-              <a href="mailto:info@info.com">info@info.com</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <div>
+        <h5 className="list-title">Links</h5>
+        <ul className="list">
+          <li>
+            <a href="/" className="list__link">
+              Overons
+            </a>
+          </li>
+          <li>
+            <a href="/" className="list__link">
+              Social meida
+            </a>
+          </li>
+          <li>
+            <a href="/" className="list__link">
+              Counters
+            </a>
+          </li>
+          <li>
+            <a href="/" className="list__link">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h5 className="list-title">Company</h5>
+        <ul className="list">
+          <li>
+            <a href="/" className="list__link">
+              Terms & conditions
+            </a>
+          </li>
+          <li>
+            <a href="/" className="list__link">
+              Privacy policy
+            </a>
+          </li>
+          <li>
+            <a href="/" className="list__link">
+              Whatever
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h5 className="list-title">Get in touch</h5>
+        <ul className="list">
+          <li>Lorem ipsum dolor sit.</li>
+          <li>085-20923-87</li>
+          <li>
+            <a href="mailto:info@info.com" className="list__link">
+              info@info.com
+            </a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 };

241089d13d2cb8030cd14cf00015c9aae2257fc2
Serge Seva <sergeseva@catalyst.net.nz>
2023-01-18 11:52:18 +1300

    Tweak all app components.

--- src/02-components/access/Access.jsx
+++ src/02-components/access/Access.jsx
@@ -7,7 +7,7 @@ const Access = () => {
       <header className="access__header">
         <h2 className="access__title">Request Early Access to Get Started</h2>
         <h3 className="access__titlesub">The possibilities are beyond your imagination</h3>
-        <p>
+        <p className="access__text">
           Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence
           way everything joy alteration boisterous the attachment. Party we years to order allow asked of.
         </p>
--- src/02-components/info/Info.jsx
+++ src/02-components/info/Info.jsx
@@ -1,7 +1,7 @@
 const Info = () => {
   return (
     <section className="info" id="info">
-      <h2 className="title-featured">What is AIO</h2>
+      <h2 className="info__feature-title title-featured">What is AIO</h2>
       <p className="info__feature-text">
         Sloop hardtack bilge water grapple belay cutlass haul wind warp barque skysail. Fire ship Gold Road Pirate Round
         deadlights crow's nest snow squiffy furl booty skysail. Ho carouser reef Privateer parley league gaff gunwalls
@@ -13,21 +13,21 @@ const Info = () => {
       </a>
       <div className="info__card">
         <h4 className="info__card-title title-featured">Chatbots</h4>
-        <p>
+        <p className="info__card-text">
           Run a rig boatswain long boat code of conduct grog gaff ballast topmast chase Blimey. Tender overhaul mutiny
           fluke scourge of the seven seas driver killick no prey, no pay stern gun.
         </p>
       </div>
       <div className="info__card">
         <h4 className="info__card-title title-featured">Knowledgebase</h4>
-        <p>
+        <p className="info__card-text">
           Handsomely spyglass gabion draught Jack Ketch cog fire in the hole shrouds to go on account loaded to the
           gunwalls. Spike heave to topsail hogshead Yellow Jack plunder sloop grapple spanker fathom.
         </p>
       </div>
       <div className="info__card">
         <h4 className="info__card-title title-featured">Education</h4>
-        <p>
+        <p className="info__card-text">
           Dead men tell no tales nipper yard furl knave clap of thunder carouser Blimey crimp hornswaggle. Transom hulk
           ye brig boatswain Brethren of the Coast scallywag killick rigging maroon.
         </p>
